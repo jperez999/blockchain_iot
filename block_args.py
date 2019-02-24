50593777fdabf912f329f8e09972793d784d4344
Julio Perez <jperez@Julios-MacBook-Pro-2.local>
2019-02-23 19:54:06 -0800

    getting aws public ips

--- block_args.py
+++ block_args.py
@@ -10,7 +10,9 @@ port = 5559
 
 def get_my_ip():
     if os.environ.get('USER') == 'ec2-user':
-        return requests.get(os.environ.get('_'))
+        endpoint = os.environ.get('_')
+        log.info(endpoint)
+        return requests.get(endpoint)
     import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
