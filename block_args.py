8b360dd43d4a00d2bac1536572734314897a5da7
Julio Perez <jperez@Julios-MacBook-Pro-2.local>
2019-02-23 22:29:40 -0800

    got it working for aws and bare metal

--- block_args.py
+++ block_args.py
@@ -5,14 +5,10 @@ import requests
 
 log = logging.getLogger()
 
-port = 5559
-
 
 def get_my_ip():
     if os.environ.get('USER') == 'ec2-user':
-        endpoint = os.environ.get('_')
-        log.info(endpoint)
-        return requests.get('http://169.254.169.254/latest/meta-data/public-ipv4')
+        return requests.get('http://169.254.169.254/latest/meta-data/public-ipv4').text()
     import socket
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect(("8.8.8.8", 80))
@@ -22,5 +18,6 @@ def get_my_ip():
     return res
 
 
+port = 5559
 my_ip = get_my_ip()
 my_topic = f"{my_ip}TOPIC"
