2a806220a8086011218e3978a0bea8a8347c67a1
Julio Perez <jperez@Julios-MacBook-Pro-2.local>
2019-03-17 19:25:47 -0700

    next block fix

--- listening.py
+++ listening.py
@@ -49,7 +49,7 @@ def get_index_release():
 
 def my_block_next():
     bc = blockChain.get_instance()
-    if bc.current_block + 1 == bc.broad_block_num:
+    if int(bc.current_block) + 1 == int(bc.broad_block_num):
         # it is my turn broadcast it
         return True
     return False
